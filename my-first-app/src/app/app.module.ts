294d9484b0a9125577bea10344ff17c286f40796
Di Chen <chendi1025@gmail.com>
2019-01-20 22:01:15 -0500

    add event/properties/twoway binding

--- my-first-app/src/app/app.module.ts
+++ my-first-app/src/app/app.module.ts
@@ -1,15 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms'
+// import { FormsModule } from '@angular/forms'
 import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
+import { FormsModule } from '@angular/forms'; 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    ServersComponent
   ],
   imports: [
-    BrowserModule,
-    FormsModule
+    BrowserModule
+    ,FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
