cf389b50cc918be1e19b90862515886517914cc6
Di Chen <chendi1025@gmail.com>
2019-01-24 17:13:03 -0500

    add structured components

--- my-first-app/src/app/server/server.component.ts
+++ my-first-app/src/app/server/server.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms'; 
+
 @Component({
     selector:'app-server',
     templateUrl:'./server.component.html',
--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -11,6 +11,7 @@ export class ServersComponent implements OnInit {
   serverCreationStatus = 'No server was created!';
   serverName = 'Test';
   serverCreated = false;
+  servers = ['Testserver','Testserver 2'];
 
   constructor() { 
     setTimeout(()=>{this.allowNewServer=true;},2000)
@@ -20,6 +21,7 @@ export class ServersComponent implements OnInit {
   }
   onCreateServer(){
     this.serverCreated =true;
+    this.servers.push(this.serverName)//push a new server on it
     this.serverCreationStatus='Server was created! The server name is '+ this.serverName;
   }
 
